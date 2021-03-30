8b77c752a8923eaea26875344374def0f4c4ea1e
taery <lavrenko.a@gmail.com>
2021-03-30 23:43:30 +0300

    new RN script

--- new.py
+++ new.py
@@ -4,7 +4,8 @@ from github import Github
 
 RELEASE_NOTES_MIN_LENGTH = 10
 PULL_REQUEST_PATTERN = re.compile(
-    '\*\*Задача\*\*:(.*)\*\*Коротко для Release Notes, в формате «Сделали/Добавили/Исправили N»\*\*:(.*)\*\*Описание\*',
+   # '\*\*Задача\*\*:(?P<issues>.*)\[(?P<is_public>[\s|x])\] рассказать пользователям(.*)\*\*Коротко для Release Notes, в формате «Сделали/Добавили/Исправили N»\*\*:(?P<rn_text>.*)\*\*Описание\*\*:',
+   '\*\*Задача\*\*:(?P<issues>.*)\s\-\s\[(?P<is_public>.*)\] рассказать пользователям(.*)\*\*Коротко для Release Notes, в формате «Сделали/Добавили/Исправили N»\*\*:(?P<rn_text>.*)\*\*Описание\*\*:',
     re.MULTILINE | re.DOTALL)
 
 GITHUB_ACCESS_TOKEN = ''
@@ -14,7 +15,7 @@ GITHUB_REPO = 'bioinf/edy'
 def get_issues(access_token, repo_name):
     stepik_repo = Github(access_token).get_repo(repo_name)
     print(list(stepik_repo.get_milestones()))
-    upcoming_milestone = stepik_repo.get_milestones()[0]
+    upcoming_milestone = stepik_repo.get_milestone(150)
     return stepik_repo.get_issues(state='closed', milestone=upcoming_milestone)
 
 
@@ -22,17 +23,21 @@ def get_release_note(issue, issue_pattern):
     search = issue_pattern.search(issue.body)
 
     if search:
-        yt_issue = search.group(1).strip()
-        release_notes = search.group(2).strip()
+        yt_issue = search.group('issues').strip()
+        release_notes = search.group('rn_text').strip()
+        is_public = bool(search.group('is_public').strip())
     else:
         print('Failed to parse', issue.pull_request.html_url, issue.body)
         yt_issue = '!!!!!!YouTrack issue!!!!!!'
         release_notes = '?????Release notes?????'
+        is_public = False
 
     if len(release_notes) < RELEASE_NOTES_MIN_LENGTH:
         return
 
-    return '1. [#{}]({}) ({}) {}\n'.format(issue.number, issue.pull_request.html_url, yt_issue, release_notes)
+    return '1. [#{}]({}){} ({}) {}\n'.format(issue.number,
+                                             issue.pull_request.html_url,
+                                             '🔥' if is_public else '',  yt_issue, release_notes)
 
 
 output_file = open('result.txt', 'w')
